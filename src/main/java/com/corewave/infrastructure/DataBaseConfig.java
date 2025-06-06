1c81cbb65d4719b3e725f181eebd117a49979fe4
ehojonv <joaovinicius.alvesaraujo@gmail.com>
2025-06-06 10:59:54 -0300

    mudando database

--- src/main/java/com/corewave/infrastructure/DataBaseConfig.java
+++ src/main/java/com/corewave/infrastructure/DataBaseConfig.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DataBaseConfig {
     public static final String URL = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL";
-    public static final String USER = "rm559369";
-    public static final String PASSWORD = "121104";
+    public static final String USER = "rm560445";
+    public static final String PASSWORD = "031005";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
